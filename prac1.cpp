bf7494b14d86f42fca0075cdfad80cabb6c525e7
VoidVedh <vedh1440@gmail.com>
2026-04-26 00:37:14 +0530

    Updated prac1.cpp

--- prac1.cpp
+++ prac1.cpp
@@ -7,5 +7,13 @@
 using namespace std;
 int main(){
     char c = 'A';
+    for(int i = 1; i < 4; i ++){
+        for(int j = 1 ; j<4;j++){
+            cout<<c;
+            c++;
+        }
+        cout<<endl;
+    }
+    return 0;
 
 }
