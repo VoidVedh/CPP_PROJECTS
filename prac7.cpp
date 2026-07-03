c9e2dcce0ff70fa1e49b78f8455fd446d0483058
XericEz <naikvedh2@gmail.com>
2026-07-03 16:46:23 +0530

    updated with bubble sort

--- prac7.cpp
+++ prac7.cpp
@@ -1,28 +1,28 @@
+
 //Auth Ved 
 //Topic : Bubble sort in ascending order 
-
 #include<iostream>
 #include<algorithm>
 using namespace std;
+void bubble_sort(int arr[], int n ){
+for (int i = 0 ; i<n;i++)
+{
+for (int j = 0 ; j<n-i-1; j++){
+if(arr[j]>arr[j+1]){
+swap(arr[j],arr[j+1]);
+}
+}
+}
+}
 
-void bubble(int arr[],int n ){
-    for(int i = 0 ; i<n-1; i++){
-      for(int j = 0; j<n-i-1; j++){
-        if(arr[j]>arr[j+1]){
-          swap(arr[j],arr[j+1])
-        }
-      }
-    }
+void print(int arr[], int n){
+for (int i =0 ; i<n ; i++){
+cout<<arr[i]<<endl;
 }
- 
-void printout(int arr[], int n ){
-      for(int i = 0 ; i<n-1; i++){
-   cout<<arr[i]<<" ";
 }
-cout<<endl;
-}
-int main(int arr[], int n){
-  n = 5;
-  arr={6,2,9,1,4};
-  bubble(arr,)
+int main(){
+int arr[]= {4,6,2,6,9};
+int n = 5;
+bubble_sort(arr, n);
+print(arr, n);
 }
